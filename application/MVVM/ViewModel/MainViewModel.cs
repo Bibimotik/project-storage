481895bf6101873839a1b98b10b3dacae35cdd9a
Slava <kuncovs19@gmail.com>
2024-07-19 20:16:15 +0300

    add base toolkit property

---
+++ application/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,15 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace application.MVVM.ViewModel;
+
+//[ObservableObject]
+public partial class MainViewModel : ObservableObject
+{
+	[ObservableProperty]
+	private string name;
+
+	public MainViewModel()
+	{
+		Name = "bibimotik";
+	}
+}
