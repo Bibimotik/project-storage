21173347c4261f1fe3bcdc030a0a0cc94429d683
Slava <kuncovs19@gmail.com>
2024-07-19 20:06:03 +0300

    recreated project

--- application/App.xaml.cs
+++ application/App.xaml.cs
@@ -1,52 +1,12 @@
-﻿using System.Windows;
-using application.MVVM.View;
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
 
-namespace application
+namespace application;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
 {
-	/// <summary>
-	/// Логика взаимодействия для App.xaml
-	/// </summary>
-	public partial class App : Application
-	{
-
-		private Window _currentWindow;
-		
-		
-		protected override void OnStartup(StartupEventArgs e)
-		{
-			base.OnStartup(e);
-
-			ShowAuth();
-			// ShowMain();
-		}
-		
-		private void ShowMain()
-		{
-			MainWindow mainWindow = new MainWindow();
-			mainWindow.Show();
-
-			CloseCurrentWindow();
-
-			_currentWindow = mainWindow;
-		}
-
-		private void ShowAuth()
-		{
-			AuthView authWindow = new AuthView();
-			authWindow.Show();
-
-			CloseCurrentWindow();
-
-			_currentWindow = authWindow;
-		}
-
-		private void CloseCurrentWindow()
-		{
-			if (_currentWindow != null)
-			{
-				_currentWindow.Close();
-				_currentWindow = null;
-			}
-		}
-	}
 }
--- application/MainWindow.xaml.cs
+++ application/MainWindow.xaml.cs
@@ -1,15 +1,23 @@
-﻿using System.Windows;
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
 
-namespace application
+namespace application;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
 {
-	/// <summary>
-	/// Логика взаимодействия для MainWindow.xaml
-	/// </summary>
-	public partial class MainWindow : Window
+	public MainWindow()
 	{
-		public MainWindow()
-		{
-			InitializeComponent();
-		}
+		InitializeComponent();
 	}
 }
