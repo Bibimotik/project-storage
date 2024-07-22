c37bb458854e66640bddb900344f4e5198125652
Mikhail <144282974+Bibimotik@users.noreply.github.com>
2024-07-22 15:12:15 +0300

    Update Tables.sql
    fix types of data

--- database/Tables.sql
+++ database/Tables.sql
@@ -1,15 +1,13 @@
--- uuid or serial?
-
 CREATE TABLE COMPANY
 (
 	CompanyID     uuid            NOT NULL PRIMARY KEY,
 	INN           char(12) UNIQUE NOT NULL,
 	KPP           char(12) UNIQUE NOT NULL,
-	OGRN          char(13)        NOT NULL,
+	OGRN          char(13) UNIQUE NOT NULL,
 	FullName      varchar(100)    NOT NULL,
 	ShortName     varchar(100)    NOT NULL,
-	LegalAddress  varchar(100)    NOT NULL,
-	PostalAddress varchar(100)    NOT NULL,
+	LegalAddress  varchar(1000)    NOT NULL,
+	PostalAddress varchar(1000)    NOT NULL,
 	Director      text            NOT NULL,
 	IsDeleted     bool            NOT NULL
 );
@@ -58,13 +56,13 @@ CREATE TABLE COUNTERPARTY
 	CounterpartyID uuid            NOT NULL PRIMARY KEY,
 	INN            char(12) UNIQUE NOT NULL,
 	KPP            char(12) UNIQUE NOT NULL,
-	OGRN           char(13)        NOT NULL,
+	OGRN           char(13) UNIQUE NOT NULL,
 	FullName       varchar(100)    NOT NULL,
 	ShortName      varchar(100)    NOT NULL,
 	Address        varchar(100)    NOT NULL,
-	PaymentAccount varchar(100)    NOT NULL, -- ?
-	CorAccount     varchar(100)    NOT NULL, -- ?
-	BIK            varchar(100)    NOT NULL, -- ?
+	PaymentAccount char(20)        NOT NULL, -- 20 символов
+	CorAccount     char(20)        NOT NULL, -- 20 символов
+	BIK            varchar(11)     NOT NULL, -- размер от 8 до 11
 	Bank           varchar(100)    NOT NULL, -- ?
 	IsDeleted      bool            NOT NULL
 );
@@ -72,7 +70,7 @@ CREATE TABLE COUNTERPARTY
 CREATE TABLE COMPANY_COUNTERPARTY
 (
 
-	CompanyCounterpartyID serial NOT NULL UNIQUE PRIMARY KEY, -- serial?
+	CompanyCounterpartyID serial NOT NULL UNIQUE PRIMARY KEY,
 	CompanyID             uuid   NOT NULL REFERENCES COMPANY (CompanyID),
 	CounterpartyID        uuid   NOT NULL REFERENCES COUNTERPARTY (CounterpartyID)
 );
@@ -87,20 +85,20 @@ CREATE TABLE CURRENCY
 CREATE TABLE COMPANY_CURRENCY
 (
 	CompanyCurrencyID serial       NOT NULL PRIMARY KEY,
-	CompanyID         uuid         NOT NULL REFERENCES COMPANY (CompanyID),
+	CompanyID         serial       NOT NULL REFERENCES COMPANY (CompanyID),
 	CurrencyID        varchar(100) NOT NULL REFERENCES CURRENCY (CurrencyID)
 );
 
 CREATE TABLE MARKING
 (
 	MarkingID serial       NOT NULL PRIMARY KEY,
-	Title     varchar(100) NOT NULL,
+	Title     varchar(50)  NOT NULL,
 	CompanyID uuid         NOT NULL REFERENCES COMPANY (CompanyID)
 );
 
 CREATE TABLE PROJECT
 (
-	ProjectID uuid         NOT NULL PRIMARY KEY, -- what type???
+	ProjectID serial       NOT NULL PRIMARY KEY,
 	Title     varchar(100) NOT NULL,
 	CompanyID uuid         NOT NULL REFERENCES COMPANY (CompanyID),
 	OrderID   uuid         NOT NULL REFERENCES "order" (OrderID),
@@ -115,27 +113,27 @@ CREATE TABLE "order"
 	CompanyCurrencyID     serial       NOT NULL REFERENCES COMPANY_CURRENCY (CompanyCurrencyID),
 	CompanyStorageID      uuid         NOT NULL REFERENCES COMPANY_STORAGE (CompanyStorageID),
 	ContractID            uuid         NOT NULL REFERENCES CONTRACT (ContractID),
-	PlanDateShipment      date         NOT NULL, -- ?
-	ShippingAddress       varchar(100) NOT NULL, -- ?
-	ApplicationDate       date         NOT NULL, -- ?
-	DeliveryPoint         varchar(100) NOT NULL, -- ?
-	DeliveryAddress       varchar(100) NOT NULL, -- ?
-	PlanDateReceipt       date         NOT NULL, -- ?
+	PlanDateShipment      date         NOT NULL, -- надо чтобы время тоже было
+	ShippingAddress       varchar(100) NOT NULL,
+	ApplicationDate       date         NOT NULL, -- надо чтобы время тоже было
+	DeliveryPoint         varchar(100) NOT NULL,
+	DeliveryAddress       varchar(100) NOT NULL,
+	PlanDateReceipt       date         NOT NULL, -- надо чтобы время тоже было
 	CompanyTransporterID  uuid         NOT NULL REFERENCES COMPANY_TRANSPORTER (CompanyTransporterID),
-	StatusID              uuid         NOT NULL REFERENCES STATUS (StatusID),
-	MarkingID             uuid         NOT NULL REFERENCES MARKING (MarkingID),
+	StatusID              serial       NOT NULL REFERENCES STATUS (StatusID),
+	MarkingID             serial       NOT NULL REFERENCES MARKING (MarkingID),
 	Comment               text         NOT NULL,
 	IsDeleted             bool         NOT NULL
 );
 
 CREATE TABLE ORDER_PRODUCT
 (
-	OrderProductID   uuid         NOT NULL PRIMARY KEY,
-	OrderID          uuid         NOT NULL REFERENCES "order" (OrderID),
-	CompanyProductID uuid         NOT NULL REFERENCES COMPANY_PRODUCT (CompanyProductID),
-	Count            int          NOT NULL,
-	VAT              varchar(100) NOT NULL, -- ?
-	Discount         double precision       -- not null? what type???
+	OrderProductID   uuid             NOT NULL PRIMARY KEY,
+	OrderID          uuid             NOT NULL REFERENCES "order" (OrderID),
+	CompanyProductID uuid             NOT NULL REFERENCES COMPANY_PRODUCT (CompanyProductID),
+	Count            int              NOT NULL,
+	VAT              double precision NOT NULL, -- налог в числах
+	Discount         double precision NOT NULL  -- может быть 0 но не null
 );
 
 CREATE TABLE STATUS
@@ -147,20 +145,20 @@ CREATE TABLE STATUS
 CREATE TABLE PRODUCT
 (
 	ProductID uuid             NOT NULL PRIMARY KEY,
-	Code      varchar(20)      NOT NULL, -- ?
+	Code      varchar(100)     NOT NULL, -- просто кодовое название 
 	Title     varchar(100)     NOT NULL,
-	Unit      varchar(20)      NOT NULL, -- ?
+	Unit      varchar(20)      NOT NULL, -- литры, кг, штуки и тд
 	Price     double precision NOT NULL,
 	IsDeleted bool             NOT NULL
 );
 
 CREATE TABLE COMPANY_PRODUCT
 (
-	CompanyProductID     uuid         NOT NULL PRIMARY KEY, -- serial?
+	CompanyProductID     serial       NOT NULL PRIMARY KEY,
 	CompanyID            uuid         NOT NULL REFERENCES COMPANY (CompanyID),
 	ProductID            uuid         NOT NULL REFERENCES PRODUCT (ProductID),
 	CompanyStorageID     uuid         NOT NULL REFERENCES COMPANY_STORAGE (CompanyStorageID),
-	AvailableForShipment int          NOT NULL,             -- ?
+	AvailableForShipment int          NOT NULL,             -- наверное лучше double потому что например литров может быть не ровно
 	InShippingArea       int          NOT NULL,             -- ?
 	InReserve            int          NOT NULL,             -- ?
 	Party                varchar(100) NOT NULL,             -- ?
