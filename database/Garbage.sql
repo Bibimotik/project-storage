a42981b732116ec5bfd2fb596abfaa643c7164cb
slava <kuncovs19@gmail.com>
2024-08-09 20:46:15 +0300

    update the sql tables according to the scheme.
    changed the order of the tables to create all of them at the same time

---
+++ database/Garbage.sql
@@ -0,0 +1,8 @@
+INSERT INTO "user" (user_id, nickname, firstname, secondname, thirdname, phone, email, password, logo, is_deleted)
+	VALUES ('42E136F9-C325-4310-AA62-962A0B0E1ED5', 'slava', 'Slava', 'Kuntsou', 'haha', '+375293647671',
+			'1@gmail.com', '1', NULL, FALSE);
+INSERT INTO "user" (user_id, nickname, firstname, secondname, thirdname, phone, email, password, logo, is_deleted)
+	VALUES ('FB945E5F-49B5-4629-8292-C6C783F25D1B', 'misha', 'Misha', 'Ржавый', 'haha', '+375292345417',
+			'q@gmail.com', 'q', NULL, FALSE);
+
+SELECT * FROM "user";
