d7eaef64d76c2bac87bc03959889cd61eb571803
slava <kuncovs19@gmail.com>
2024-10-05 22:44:59 +0300

    feat: create simple web api

---
+++ server/Storage.API/Controllers/Controller.cs
@@ -0,0 +1,17 @@
+﻿using System.Diagnostics;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace Storage.API.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class Controller
+{
+	[HttpGet(nameof(Get))]
+	public int Get()
+	{
+		Debug.WriteLine("qwe");
+		return 123;
+	}
+}
